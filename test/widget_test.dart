163bf4557932e3bdcba00fca67a9ee464c78c389
Teresiah-cpu <terrywambo2018@gmail.com>
2024-05-18 20:29:32 +0300

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shopeasy/main.dart';
+import 'package:Ecommerce_G10/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
