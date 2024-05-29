c48ff01382b46a199e09219143bc3bd5d12a8cf7
Teresiah-cpu <terrywambo2018@gmail.com>
2024-05-29 09:28:45 +0300

    adjusted the get started button and cart logo size

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -10,73 +10,77 @@ class IntroScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[50],
       body: SafeArea(
-        child: Column(
-          children: [
-            // big logo
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 30.0,
-                right: 20.0,
-                top: 70,
-                bottom: 20,
-              ),
-              child: Image.asset('images/cartlogo.png'),
-            ),
-
-            // we deliver at your doorstep
-            Padding(
-              padding: const EdgeInsets.all(28.0),
-              child: Text(
-                'We deliver products at your doorstep',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.notoSerif(
-                    fontSize: 36, fontWeight: FontWeight.bold),
-              ),
-            ),
-
-            // 
-            Text(
-              'Quality Products',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[700],
-              ),
-            ),
-
-            const SizedBox(height: 24),
-
-            const Spacer(),
-
-            // get started button
-            GestureDetector(
-              onTap: () => Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return SignupPage();
-                  },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // big logo
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 30.0,
+                  right: 20.0,
+                  top: 70,
+                  bottom: 20,
                 ),
+                child: Image.asset('images/cartlogo.png'),
               ),
-              child: Container(
-                padding: const EdgeInsets.all(24),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: const Color.fromARGB(255, 112, 91, 222),
-                ),
-                child: const Text(
-                  "Get Started",
-                  style: TextStyle(
-                    color: Colors.white,
-                    // fontWeight: FontWeight.bold,
-                    fontSize: 16,
+
+              // we deliver Products at your doorstep
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Text(
+                  'We deliver products at your doorstep',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.notoSerif(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ),
 
-            const Spacer(),
-          ],
+              // Quality Products
+              Text(
+                'Quality Products',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[700],
+                ),
+              ),
+
+              const SizedBox(height: 24),
+
+              const SizedBox(height: 50), // space before the button
+
+              // get started button
+              GestureDetector(
+                onTap: () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignupPage();
+                    },
+                  ),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: const Color.fromARGB(255, 112, 91, 222),
+                  ),
+                  child: const Text(
+                    "Get Started",
+                    style: TextStyle(
+                      color: Colors.white,
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 50), //  space after the button
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -39,4 +39,7 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+
+
+  
 }
